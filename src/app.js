3b72010f76fb145ace25267229f8dbfc4d60b30c
Noal Brown <noalbrown1@gmail.com>
2020-11-22 12:55:48 -0600

    changes

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
+import { http } from './http';
 
+document.addEventListener('DOMContentLoaded', getPosts);
+
+function getPosts() {
+  http.get('http://localhost:3000/posts')
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+}
