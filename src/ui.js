c802122fc483685e83b6240f266b8d8e1d902061
Noal Brown <noalbrown1@gmail.com>
2020-11-22 17:02:11 -0600

    finish

--- src/ui.js
+++ src/ui.js
@@ -25,6 +25,79 @@ class UI {
     });
     this.post.innerHTML = output;
   }
+
+  showAlert(message, className) {
+    this.clearAlert();
+
+    const div = document.createElement('div')
+
+    div.className = className;
+
+    div.appendChild(document.createTextNode(message));
+
+    const container = document.querySelector('.postsContainer');
+
+    const posts = document.querySelector('#posts');
+
+    container.insertBefore(div, posts)
+
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+
+    if (currentAlert) {
+      currentAlert.remove();
+    }
+  }
+
+  clearFields() {
+    this.titleInput.value = '';
+    this.bodyInput.value = '';
+  }
+
+  fillForm(data) {
+    this.titleInput.value = data.title;
+    this.bodyInput.value = data.body;
+    this.idInput.value = data.id;
+
+    this.changeFormState('edit')
+  }
+
+  clearIdInput() {
+    this.idInput.value = '';
+  }
+
+  changeFormState(type) {
+    if (type === 'edit') {
+      this.postSubmit.textContent = 'Update Post';
+      this.postSubmit.className = 'post-submit btn btn-warning btn-block';
+
+      const button = document.createElement('button');
+      button.className = 'post-cancel btn btn-light btn-block';
+      button.appendChild(document.createTextNode('Cancel Edit'))
+
+      const cardForm = document.querySelector('.card-form');
+
+      const formEnd = document.querySelector('.form-end');
+
+      cardForm.insertBefore(button, formEnd);
+    } else {
+      this.postSubmit.textContent = 'Post it';
+      this.postSubmit.className = 'post-submit btn btn-warning btn-primary';
+
+      if (document.querySelector('.post-cancel')) {
+        document.querySelector('.post-cancel').remove();
+      }
+
+      this.clearIdInput();
+
+      this.clearFields();
+    }
+  }
 }
 
 export const ui = new UI();
