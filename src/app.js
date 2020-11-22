c3593c2bf326201a64f345c1a5f7ade64922fd11
Noal Brown <noalbrown1@gmail.com>
2020-11-22 12:29:36 -0600

    changes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,1 @@
-// Common JS module syntax
-// const person = require('./mymodule1');
 
-// ES2015 Module
-// import { person, sayHello } from './mymodule2'
-// import * as mod from './mymodule2'
-import greeting from './mymodule2'
-
-console.log(person.name)
-console.log(mod.sayHello())
-console.log(greeting)
--- src/mymodule1.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  name: 'Noal',
-  email: 'test@test.com'
-}
